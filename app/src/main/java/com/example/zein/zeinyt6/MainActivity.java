34982954a048c0ed96be6e101619442f4772c37c
gzeinnumer <gzeinnumer@github.com>
2018-12-15 23:30:23 +0700

    master

---
+++ app/src/main/java/com/example/zein/zeinyt6/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.zein.zeinyt6;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.ebanx.swipebtn.OnStateChangeListener;
+import com.ebanx.swipebtn.SwipeButton;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        SwipeButton swipeButton = (SwipeButton)findViewById(R.id.swipeBtn);
+
+        swipeButton.setOnStateChangeListener(new OnStateChangeListener() {
+            @Override
+            public void onStateChange(boolean active) {
+                Toast.makeText(MainActivity.this, "Active : "+active,Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
